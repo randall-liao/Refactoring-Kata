dcb99ab8ddc7bba506dd5dc05830cf9cca29961d
Randall (Yichen) Liao <36297267+liaoyichen123@users.noreply.github.com>
2025-11-21 23:34:44 -0500

    feat: implement BackstagePassesQualityStrategy with unit tests

--- Java/src/main/java/com/gildedrose/item/strateties/BackstagePassesQualityStrategy.java
+++ Java/src/main/java/com/gildedrose/item/strateties/BackstagePassesQualityStrategy.java
@@ -2,17 +2,24 @@ package com.gildedrose.item.strateties;
 
 import static com.gildedrose.item.Constants.MAX_ALLOWED_QUALITY;
 
-public class BackstagePassesQualityStrategy  implements  QualityStrategy
-{
+public class BackstagePassesQualityStrategy implements QualityStrategy {
+
+    private static final int INCREASE_TWO_THRESHOLD_DAYS = 10;
+    private static final int INCREASE_THREE_THRESHOLD_DAYS = 5;
+    private static final int CONCERT_DATE = 0;
+
     public int update(int currentQuality, int sellIn) {
-        if (currentQuality < MAX_ALLOWED_QUALITY && currentQuality > 0) {
-            if (sellIn < 0) {
-                currentQuality =0;
-            } else if (sellIn <=5 ) {
+        if (currentQuality > 0) {
+            if (sellIn < CONCERT_DATE) {
+                currentQuality = 0;
+            } else if (sellIn <= INCREASE_THREE_THRESHOLD_DAYS) {
                 currentQuality += 3;
-            } else if (sellIn <= 10) {
-                currentQuality = 2;
+            } else if (sellIn <= INCREASE_TWO_THRESHOLD_DAYS) {
+                currentQuality += 2;
             }
+        }
+        if (currentQuality > MAX_ALLOWED_QUALITY) {
+            currentQuality = MAX_ALLOWED_QUALITY;
         }
         return currentQuality;
     }
---
+++ Java/src/test/java/com/gildedrose/item/strateties/BackstagePassesQualityStrategyTest.java
@@ -0,0 +1,101 @@
+package com.gildedrose.item.strateties;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static com.gildedrose.item.Constants.MAX_ALLOWED_QUALITY;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BackstagePassesQualityStrategyTest {
+
+    private BackstagePassesQualityStrategy backstagePass;
+
+    @BeforeEach
+    void setUp() {
+        backstagePass = new BackstagePassesQualityStrategy();
+    }
+
+    @Test
+    void test_pass_drop_to_zero_when_expired(){
+        // arrange
+        int expectedResult = 0;
+        int currentQuality = 3;
+        int sellIn = -1;
+        // act
+        int actualResult = backstagePass.update(currentQuality, sellIn);
+        // assert
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void test_pass_drop_to_zero_with_max_quality() {
+        // arrange
+        int expectedResult = 0;
+        int sellIn = -1;
+        // act
+        int actualResult = backstagePass.update(MAX_ALLOWED_QUALITY, sellIn);
+        // assert
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void test_pass_not_exceed_max() {
+        // arrange
+        int currentQuality = MAX_ALLOWED_QUALITY - 1;
+        int sellIn = 4;
+        // act
+        int actualResult = backstagePass.update(currentQuality, sellIn);
+        // assert
+        assertEquals(MAX_ALLOWED_QUALITY, actualResult);
+    }
+
+    @Test
+    void test_quality_increase_two_within_ten() {
+        // arrange
+        int expectedResult = 7;
+        int currentQuality = 5;
+        int sellIn = 8;
+        // act
+        int actualResult = backstagePass.update(currentQuality, sellIn);
+        // assert
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void test_quality_increase_two_lower_boundary_value() {
+        // arrange
+        int expectedResult = 10;
+        int currentQuality = 8;
+        int sellIn = 6;
+        // act
+        int actualResult = backstagePass.update(currentQuality, sellIn);
+        // assert
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void test_quality_increase_two_upper_boundary_value() {
+        // arrange
+        int expectedResult = 10;
+        int currentQuality = 8;
+        int sellIn = 10;
+        // act
+        int actualResult = backstagePass.update(currentQuality, sellIn);
+        // assert
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "10, 7, 5", // upper boundary case
+        "10, 7, 0",  // lower boundary case
+        "10, 7, 3"
+    })
+    void test_quality_increase_three_cases(int expectedResult, int currentQuality, int sellIn) {
+        int actualResult = backstagePass.update(currentQuality, sellIn);
+        assertEquals(expectedResult, actualResult);
+    }
+
+}
